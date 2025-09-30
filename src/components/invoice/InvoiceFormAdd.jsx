0ff367da6ef6716c7f8c476182d0ef8572ba45ff
Eduardo-VaZu <u22234544@utp.edu.pe>
2025-09-30 17:03:20 -0500

    feat: add invoice form

--- src/components/invoice/InvoiceFormAdd.jsx
+++ src/components/invoice/InvoiceFormAdd.jsx
@@ -1,30 +1,56 @@
 import InvoiceAddItemForm from "./InvoiceAddItemForm";
 
-const InvoiceFormAdd = ({handleAddItem, setIsFormOpen, isFormOpen}) => {
-  
+const InvoiceFormAdd = ({ handleAddItem, setIsFormOpen, isFormOpen }) => {
+  const handlePrint = () => {
+    window.print();
+  };
   return (
     <>
       <div className="flex justify-between items-center mb-4">
-        <h3 className="text-xl font-bold text-gray-800">
-          Service Details
-        </h3>
+        <h3 className="text-xl font-bold text-gray-800">Service Details</h3>
+        <button
+          onClick={() => handlePrint()}
+          className="px-4 py-2 text-sm font-semibold text-white bg-gray-500 rounded-lg shadow-md hover:bg-gray-600 transition duration-150 flex items-center space-x-1"
+        >
+          <svg
+            class="w-5 h-5 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M16.444 18H19a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h2.556M17 11V5a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v6h10ZM7 15h10v4a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-4Z"
+            />
+          </svg>
+
+          <span>Print</span>
+        </button>
         <button
           onClick={() => setIsFormOpen(!isFormOpen)}
           className="px-4 py-2 text-sm font-semibold text-white bg-indigo-500 rounded-lg shadow-md hover:bg-indigo-600 transition duration-150 flex items-center space-x-1"
         >
           <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
+            class="w-5 h-5 text-gray-800 dark:text-white"
+            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            ></path>
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 12h14m-7 7V5"
+            />
           </svg>
           <span>{isFormOpen ? "Close Form" : "Add Item"} </span>
         </button>
@@ -34,7 +60,7 @@ const InvoiceFormAdd = ({handleAddItem, setIsFormOpen, isFormOpen}) => {
         <InvoiceAddItemForm
           handleAddItem={handleAddItem}
           onCancel={() => setIsFormOpen(false)}
-          currency={'USD'}
+          currency={"USD"}
         />
       )}
     </>
